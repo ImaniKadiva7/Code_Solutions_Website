586a62c70b686679ec8e1e2456c895b2e21f33b5
ImaniKadiva7 <kadivaimani@yahoo.com>
2023-02-27 10:50:42 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,146 @@
+function showBinaryGapCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/Binary-Gap-Solution-Code/main/Binary_gap_solution.txt');
+}
+
+function showCyclicRotationCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/Cyclic-Rotation-Code/main/cyclic_rotation_code.txt.txt');
+}
+
+function showOddOccurenceCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/Odd-Occurrence-In-Array/main/OddOccurrenceInArray.txt');
+}
+
+function showFrogJumpCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/FrogJump.txt');
+}
+
+function showPermMissingElem()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/PermMissingElem.txt');
+}
+
+function showTapeEquilibriumCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/TapeEquilibrium.txt');
+}
+
+function showFrogRiverOneCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/frogRiverOne.txt');
+}
+
+function showPermCheckCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/PermCheck.txt');
+}
+
+function showMaxCountersCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/MaxCounters.txt');
+}
+
+function showMissingIntegerCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/MissingIntegers.txt')
+}
+
+function showPassingCarsCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/passingCars.txt');
+}
+
+function showCountDivCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/CountDiv.txt');
+}
+
+function showGenomicRangeQueryCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/GenomicRangeQuery.txt');
+}
+
+function showMinAvgTwoSliceCode()
+{
+    open('https://raw.githubusercontent.com/ImaniKadiva7/My-Codility-Problem-code-solutions/main/MinAvgTwoSlice.txt');
+}
+
+//------------------------------------------------------------------------------------------------------------------------
+
+function showDistinctCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/Distinct.txt');
+}
+
+function showMaxProductofThreeCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/MaxProductOfThree.txt');
+}
+
+function showTriangleCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/Triangle.txt');
+}
+
+function showNumberofDiscIntersectionCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/NumberofDiscIntersections.txt');
+}
+
+function showBracketsElem()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/Brackets.txt');
+}
+
+function showFishCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/Fish.txt');
+}
+
+function showNestingCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/Nesting.txt');
+}
+
+function showStoneWallCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/StoneWall.txt');
+}
+
+function showMaxProfitCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/Max_Profit.txt');
+}
+
+function showMaxSliceSumCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/MaxSliceSum.txt')
+}
+
+function showMaxDoubleSliceSumCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/MaxDoubleSliceSum.txt');
+}
+
+function showMinMaxDivisionCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/MinMaxDivision.txt');
+}
+
+function showFirstUniqueCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/FirstUnique.txt');
+}
+
+function showTreeHeightCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/treeHeight.txt');
+}
+
+function showStrSymmetryPointCode()
+{
+    open('https://github.com/ImaniKadiva7/My-Codility-Problem-code-solutions/blob/main/StrSymmetryPoint.txt');
+}
